d236b0cb487c3583533766d2f0ba0f041b5cc5cc
아름 오 <20231147@student.sungshin.ac.kr>
2026-03-25 19:25:34 +0900

    feat:회원가입

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='customuser_permissions_set', blank=True)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
 
-# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST) #상태정보가 있는 폼
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('signup') #signup페이지로 리다이렉트
+    else:
+        form = SignUpForm() #빈 새로운 폼
+
+    return render(request, 'signup.html', {'form': form}) #기본적으로 get 메소드로 접근 = 페이지에 처음 들어가면 바로 새 폼 주어짐
