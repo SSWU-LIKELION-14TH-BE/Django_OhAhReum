915ed5d510d72dba9abf9e465e94f1003fca0160
아름 오 <20231147@student.sungshin.ac.kr>
2026-03-31 18:27:59 +0900

    feat:기능구현W2 - #2

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import signup_view
+from .views import login_view
+from .views import logout_view
+from .views import home_view
+from .views import password_reset_view
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('home/', home_view, name='home'),
+    path('logout/', logout_view, name='logout'),
+    path('password-reset/', password_reset_view, name='password_reset'),
 ]
