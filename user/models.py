e67657413480e2166873b726f7d9441b92c7ef2c
아름 오 <20231147@student.sungshin.ac.kr>
2026-04-04 20:18:16 +0900

    fix:닉네임기준으로모델수정&불필요한폴더삭제

--- user/models.py
+++ user/models.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
-    nickname = models.CharField(max_length=30, blank=True, null=True)
+    nickname = models.CharField(max_length=30, default='anonymous')
+
+    def __str__(self):
+        return self.nickname
 
     groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True)
     user_permissions = models.ManyToManyField(Permission, related_name='customuser_permissions_set', blank=True)
