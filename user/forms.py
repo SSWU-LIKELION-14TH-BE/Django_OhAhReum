ad58305d123dd8273ae24896118efaf5323a9db7
아름 오 <20231147@student.sungshin.ac.kr>
2026-04-10 22:28:20 +0900

    feat:이메일 인증으로 비밀번호 초기화 #10

--- user/forms.py
+++ user/forms.py
@@ -15,16 +15,3 @@ class LoginForm(AuthenticationForm):
 class LogoutForm(forms.Form):
     pass
 
-class PasswordResetForm(forms.Form):
-    username = forms.CharField(max_length=30)
-    email = forms.EmailField(max_length=150)
-    password1 = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
-
-    def clean(self):
-        cleaned = super().clean()
-
-        if cleaned.get('password1') != cleaned.get('password2'):
-            raise forms.ValidationError("비밀번호가 일치하지 않습니다.")
-
-        return cleaned
--- user/urls.py
+++ user/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
-from .views import signup_view
-from .views import login_view
-from .views import logout_view
-from .views import home_view
-from .views import password_reset_view
+from .views import *
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
     path('home/', home_view, name='home'),
     path('logout/', logout_view, name='logout'),
-    path('password-reset/', password_reset_view, name='password_reset'),
+    path('password-reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
